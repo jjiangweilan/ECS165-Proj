d4234e2f870a233e59a038c15d85fd2dba00c502
weilan <jjiangweilan@gmail.com>
2019-01-10 10:00:36 -0800

    Hello World

--- Proj/ViewController.swift
+++ Proj/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let helloWorld = UITextView(frame: CGRect( x: 30,y : 30, width: 100, height: 100));
+        helloWorld.text = "Hello World";
+        view.addSubview(helloWorld);
     }
 
 
