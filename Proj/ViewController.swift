e3d435ce67f425e0b297679448d816340f362ab7
weilan <jjiangweilan@gmail.com>
2019-01-10 09:29:49 -0800

    Initial Commit

---
+++ Proj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proj
+//
+//  Created by jiehong jiang on 1/10/19.
+//  Copyright © 2019 jiehong jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
