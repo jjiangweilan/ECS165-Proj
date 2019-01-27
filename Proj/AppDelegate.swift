e4f3add883c6c34b27f189049693978d8b7a2f05
weilan <jjiangweilan@gmail.com>
2019-01-26 22:57:47 -0800

    add gitgnore

--- Proj/AppDelegate.swift
+++ Proj/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        DatabaseBridge.initRef()
+        DatabaseBridge.signIn(withEmail: "jjiangweilan@gmail.com", password: "123456" , completion : nil) //123456
         
         return true
     }
